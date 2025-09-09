c66dcf8705c835aa3c3c6a0a975e98e884dadf02
lilo-error404 <murilosouzagames2005@gmail.com>
2025-09-09 11:11:38 -0300

    ajustando header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,19 +9,40 @@ import {
     NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 
+const produtos = [
+    {
+        title: "Forest",
+        href: "#",
+        description: "Ajuda a manter o foco de forma divertida."
+    },
+    {
+        title: "ClickUp",
+        href: "#",
+        description: "Organize tarefas e metas em um só lugar."
+    },
+    {
+        title: "Habitica",
+        href: "#",
+        description: "Torna a organização mais motivadora."
+    },
+
+]
+
 export default function Header() {
     return (
         <>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <NavigationMenuLink>Link</NavigationMenuLink>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
+            <header>
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <NavigationMenuLink>Link</NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </header>
         </>
 
     )
