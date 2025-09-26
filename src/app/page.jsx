a4ab8c661babfab2ed1417644f69bb2f64b1084f
lilo-error404 <murilosouzagames2005@gmail.com>
2025-09-26 08:24:51 -0300

    add block Contact

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/layout/header"
 import Container from "../components/layout/container"
+import Contact from "../components/layout/contact"
 
 export default function Home() {
   return (
     <>
       <Header />
       <Container />
+      <Contact />
     </>
   )
 }
---
+++ src/components/layout/contact.jsx
@@ -0,0 +1,9 @@
+import Contact01Page from "../contact-01/contact-01"
+
+export default function Contact() {
+    return (
+        <section id="#contact" className="bg-white">
+                <Contact01Page />
+        </section>
+    )
+}
